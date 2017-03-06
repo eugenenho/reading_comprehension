eb8c151998c2dd11a0d84b0e809e72a2d8733d0a
Brian Higgins <bhiggins@stanford.edu>
2017-03-05 23:32:32 -0800

    working, but error is astonishigly low, so probably not really working....

--- simple_model.py
+++ simple_model.py
@@ -7,20 +7,14 @@ from seq2seq.models import Seq2Seq
 from keras.layers.embeddings import Embedding
 import numpy as np
 
+import simple_configs
 
-# need to add masking
+# (need to add masking)
 def build_model():
-	GLOVE_DIR = './download/dwr/'
-	TEXT_DATA_DIR = './data/marco/vocab.dat'
-	INPUT_MAX_LENGTH = 1000
-	OUTPUT_MAX_LENGTH = 15
-	MAX_NB_WORDS = 200000
-	EMBEDDING_DIM = 100
-
 	model = Sequential()
 
-	embeddings_index = {}
-	f = open(os.path.join(GLOVE_DIR, 'glove.6B.' + str(EMBEDDING_DIM) + 'd.txt'))
+	embeddings_index = {} # word -> embedding vector
+	f = open(os.path.join(simple_configs.GLOVE_DIR, 'glove.6B.' + str(simple_configs.EMBEDDING_DIM) + 'd.txt'))
 	for line in f:
 	    values = line.split()
 	    word = values[0]
@@ -30,34 +24,36 @@ def build_model():
 	print 'built word map'
 
 	word_index = {}  # dictionary mapping label name to numeric id
-	f = open(TEXT_DATA_DIR)
+	f = open(simple_configs.TEXT_DATA_DIR)
 	for i, line in enumerate(f):
 		word_index[line] = i
 	print 'made vocabulary'
 
 	# prepare embedding matrix
-	embedding_matrix = np.zeros((MAX_NB_WORDS, EMBEDDING_DIM))
+	embedding_matrix = np.zeros((simple_configs.MAX_NB_WORDS, simple_configs.EMBEDDING_DIM))
 	for word, i in word_index.iteritems():
-	    if i >= MAX_NB_WORDS: continue
+	    if i >= simple_configs.MAX_NB_WORDS: continue
 	    embedding_vector = embeddings_index.get(word)
 	    if embedding_vector is not None:
 	        embedding_matrix[i] = embedding_vector
 	print 'Prepared embedding matrix.'
 
+	# BUILD ACTUAL MODEL
 	embedding_layer = Embedding(
-			MAX_NB_WORDS,
-		    EMBEDDING_DIM,
+			simple_configs.MAX_NB_WORDS,
+		    simple_configs.EMBEDDING_DIM,
 		    weights=[embedding_matrix],
-		    input_length=INPUT_MAX_LENGTH
+		    input_length=simple_configs.INPUT_MAX_LENGTH
 	    )
 	s2s_layer = Seq2Seq(
-		batch_input_shape=(None, INPUT_MAX_LENGTH, EMBEDDING_DIM),
-		hidden_dim=50, 
-		output_length=OUTPUT_MAX_LENGTH, 
-		output_dim=200000, 
-		depth=4
+		batch_input_shape=(None, simple_configs.INPUT_MAX_LENGTH, simple_configs.EMBEDDING_DIM),
+		hidden_dim=simple_configs.HIDDEN_DIM, 
+		output_length=simple_configs.OUTPUT_MAX_LENGTH, 
+		output_dim=simple_configs.MAX_NB_WORDS, 
+		depth=simple_configs.DEPTH
 	)
 
+	# ADD UP ACTUAL MODEL
 	model.add(embedding_layer)
 	model.add(s2s_layer)
 	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
--- simple_train.py
+++ simple_train.py
@@ -2,21 +2,17 @@ import copy
 from tqdm import tqdm 
 import numpy as np
 import cPickle
-import simple_model
 import time
 
-INPUT_MAX_LENGTH = 1000
-OUTPUT_MAX_LENGTH = 15
-TEST_PREDICTIONS_FREQUENCY = 10
-TRAIN_BATCH_SIZE = 50
-NUM_QUESTIONS = 74093
-MAX_NB_WORDS = 200000
-MAX_NUM_ITERS = int(float(NUM_QUESTIONS) / float(TRAIN_BATCH_SIZE)) + 1
-FULL_PASS_ITERS = 10
+import simple_model
+import simple_configs
 
 def save_model(nn_model):
     nn_model.save_weights('./model_save', overwrite=True)
 
+# This is the main function for the model right now.
+# Builds model from the other file, gets data, and runs a fit
+# Then saves model weights
 def train_model():
     nn_model = simple_model.build_model()
 
@@ -25,15 +21,15 @@ def train_model():
 
     start_time = time.time()
     sents_batch_iteration = 1
-    for iter_num in range(1, FULL_PASS_ITERS + 1):
+    for iter_num in range(1, simple_configs.FULL_PASS_ITERS + 1):
         print 'Full-data-pass iteration num: ' + str(iter_num)
 
-        for i in tqdm(range(1, MAX_NUM_ITERS + 1)):
-            X_batch = X_train[i:(i + 1)*TRAIN_BATCH_SIZE]
-            Y_batch = Y_train[i:(i + 1)*TRAIN_BATCH_SIZE, :]
-            nn_model.fit(X_batch, Y_batch, batch_size=TRAIN_BATCH_SIZE, nb_epoch=1, verbose=1)
-
-            if sents_batch_iteration % TEST_PREDICTIONS_FREQUENCY == 0:
+        # FIT
+        for i in tqdm(range(1, simple_configs.MAX_NUM_ITERS + 1)):
+            X_batch = X_train[i:(i + 1)*simple_configs.TRAIN_BATCH_SIZE]
+            Y_batch = Y_train[i:(i + 1)*simple_configs.TRAIN_BATCH_SIZE, :]
+            nn_model.fit(X_batch, Y_batch, batch_size=simple_configs.TRAIN_BATCH_SIZE, nb_epoch=1, verbose=1)
+            if sents_batch_iteration % simple_configs.TEST_PREDICTIONS_FREQUENCY == 0:
                 save_model(nn_model)
 
             sents_batch_iteration += 1
@@ -44,43 +40,54 @@ def train_model():
     save_model(nn_model)
 
 
+# This constructs the data from the pickled objects
 def build_data():
-    X_train = np.zeros((NUM_QUESTIONS, INPUT_MAX_LENGTH))
+    # X DATA...
+    X_train = np.zeros((simple_configs.NUM_QUESTIONS, simple_configs.INPUT_MAX_LENGTH))
     train_passages = cPickle.load(open("./data/marco/train.ids.passage.pkl","rb"))
     train_questions = cPickle.load(open("./data/marco/train.ids.question.pkl", "rb" ))
 
+    # FOR EACH PASSAGE, CONCATENATE [QUESTION, P1, P2, ..., PN]
+    # pad to len 1000 with zeros (still need to figure out masking in the model)
     for i, tp in enumerate(train_passages):
         curr_input_list = train_questions[i]
         for p in tp:
             curr_input_list.extend(p)
 
-        # remove words witout embeddings
+        # remove words witout embeddings (really uncommon words)
         for w in reversed(curr_input_list):
-            if int(w) > MAX_NB_WORDS: curr_input_list.remove(w)
+            if int(w) > simple_configs.MAX_NB_WORDS: curr_input_list.remove(w)
 
-        if len(curr_input_list) < INPUT_MAX_LENGTH:
-            curr_input_list.extend( [0] * (INPUT_MAX_LENGTH - len(curr_input_list)) )
+        # padding
+        if len(curr_input_list) < simple_configs.INPUT_MAX_LENGTH:
+            curr_input_list.extend( [0] * (simple_configs.INPUT_MAX_LENGTH - len(curr_input_list)) )
 
-        X_train[i] = np.array(curr_input_list[:INPUT_MAX_LENGTH])
+        X_train[i] = np.array(curr_input_list[:simple_configs.INPUT_MAX_LENGTH])
 
-    Y_train_indexes = np.zeros((NUM_QUESTIONS, OUTPUT_MAX_LENGTH))
+    # Y DATA....
     answers_list = cPickle.load(open("./data/marco/train.ids.answer.pkl","rb" ))
+
+    Y_train_indexes = np.zeros((simple_configs.NUM_QUESTIONS, simple_configs.OUTPUT_MAX_LENGTH))
     for i, ans in enumerate(answers_list):
+        # weird thing here, the answer is stores as a list of lists
         ans = ans[0] if len(ans) == 1 else []
+        # remove words witout embeddings (really uncommon words)
         for w in reversed(ans): 
-            if int(w) > MAX_NB_WORDS: ans.remove(w)
-        if len(ans) < OUTPUT_MAX_LENGTH: ans.extend( [0] * (OUTPUT_MAX_LENGTH - len(ans)) )
-        Y_train_indexes[i] = np.array(ans[:OUTPUT_MAX_LENGTH])
+            if int(w) > simple_configs.MAX_NB_WORDS: ans.remove(w)
+        # pad / truncate values
+        if len(ans) < simple_configs.OUTPUT_MAX_LENGTH: ans.extend( [0] * (simple_configs.OUTPUT_MAX_LENGTH - len(ans)) )
+        # add to matrix
+        Y_train_indexes[i] = np.array(ans[:simple_configs.OUTPUT_MAX_LENGTH])
 
     Y_train_indexes = Y_train_indexes.astype(int)
-    Y_train = np.zeros((NUM_QUESTIONS, OUTPUT_MAX_LENGTH, MAX_NB_WORDS))
 
+    # Now we need to convert this into a matrix of 1-hot vectors.  So it will be 3D with dimentions below
+    Y_train = np.zeros((simple_configs.NUM_QUESTIONS, simple_configs.OUTPUT_MAX_LENGTH, simple_configs.MAX_NB_WORDS))
     for r, row in enumerate(Y_train_indexes):
         for c, i in enumerate(row):
-            if i > MAX_NB_WORDS: print r, c, i
             Y_train[r][c][i] = 1
 
-    print 'Made Data'
+    print 'Made Data Matrices'
     return X_train, Y_train
 
 
