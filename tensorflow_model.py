6515933b5e3f59fd28d673cb91c360b9b0cb1fee
Brian Higgins <bhiggins@stanford.edu>
2017-03-16 00:45:27 -0700

    done for the night, ran on small data set and struggled hard

--- tensorflow_model.py
+++ tensorflow_model.py
@@ -138,10 +138,6 @@ class TFModel():
             prog.update(i + 1, [("train loss", loss)])
 
             batch = data.get_batch()
-            if i % 1200 == 0 and i > 0:
-                self.log.write('\nNow saving file...')
-                saver.save(sess, './data/model.weights')
-                self.log.write('\nSaved...')
             i += 1
         return losses
 
