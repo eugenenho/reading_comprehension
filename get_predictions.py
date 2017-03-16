96e73d0f136702031cd5e0e1d5f0314d9d7697ac
Brian Higgins <bhiggins@stanford.edu>
2017-03-15 21:57:53 -0700

    json generator

--- get_predictions.py
+++ get_predictions.py
@@ -14,14 +14,12 @@ def get_predictions(data, embeddings):
 	with tf.Graph().as_default():
 			start = time.time()
 			model = TFModel(embeddings)
-			model.log.write("\nBuild graph took " + str(time.time() - start) + " seconds")
+			model.log.write("\nRebuild graph took " + str(time.time() - start) + " seconds")
 			init = tf.global_variables_initializer()
 			with tf.Session() as session:
 				session.run(init)
-				model.log.write('\nStarting to restore')
 				saver = tf.train.import_meta_graph("./data/Models/model.weights.meta")
 				saver.restore(session, './data/Models/model.weights')
-				model.log.write('\nran rebuild, ready to predict')
 				predictions = model.predict(session, saver, data)
 	model.log.close()
 	return predictions
@@ -39,7 +37,7 @@ def get_ground_truth(data, index_word):
 
 
 def get_index_word_dict():
-	index_word = {}  # dictionary mapping label name to numeric id
+	index_word = dict()
 	f = open(TEXT_DATA_DIR)
 	for i, line in enumerate(f):
 		word = line.lower().strip()
@@ -47,20 +45,16 @@ def get_index_word_dict():
 	return index_word
 
 def sub_in_word(preds, index_word):
-	print len(preds)
 	word_preds = list()
 
 	for batch in preds:
-		print batch.shape
 		for row in batch:
-			print row
 			ans = list()
 			for i in row:
 				ans.append(index_word[i])
 			ans = ' '.join(ans)
 			word_preds.append(ans)
 
-	print '\n\n', word_preds, '\n\n', 'word_preds len', len(word_preds)
 	return word_preds
 
 def build_json_file(preds, file_name):
@@ -69,7 +63,6 @@ def build_json_file(preds, file_name):
 		curr_ans = dict()
 		curr_ans["answers"] = [p]
 		curr_ans["query_id"] = i
-		print json.dumps(curr_ans)
 		f.write( json.dumps(curr_ans) )
 		f.write('\n')
 	f.close()
@@ -77,19 +70,14 @@ def build_json_file(preds, file_name):
 if __name__ == "__main__":
 	embeddings = EmbeddingHolder().get_embeddings_mat()
 	data = TFDataHolder('val', PRED_BATCH_SIZE)
-
-	print 'starting preds'
-	preds = get_predictions(data, embeddings)
-
-	print 'got predictions\n Get index_word'
+	
 	index_word = get_index_word_dict()
-	print 'got dict \n sub_in_words'
-	preds = sub_in_word(preds, index_word)
 
+	preds = get_predictions(data, embeddings)
+	preds = sub_in_word(preds, index_word)
 	build_json_file(preds, 'val_preds_json.json')
-	print 'built preds'
+	
 	get_ground_truth(data, index_word)
-	print 'got ground truth'
 
 
 
