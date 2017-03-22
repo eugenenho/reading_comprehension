18f37b7d62500e0c5011689be008aaa742c70e67
Brian Higgins <bhiggins@stanford.edu>
2017-03-22 00:38:56 -0700

    updating configs for gpu run of classifier

--- simple_configs.py
+++ simple_configs.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
 # DATA PARAMETERS
-NUM_EPOCS = 5
+NUM_EPOCS = 10
 LEARNING_RATE = 0.001
-DROPOUT = 1
+DROPOUT = 0.5
 SMALL_DATA_SET = False
-MAX_DATA_SIZE = 500 #-1 to set no limit on data size
+MAX_DATA_SIZE = -1 #-1 to set no limit on data size
 TRAIN_BATCH_SIZE = 128
 MAX_GRAD_NORM = 40
 
