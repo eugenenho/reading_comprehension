8cefbca92dca094453075ceb8e8a2ec3c4422be5
Brian Higgins <bhiggins@stanford.edu>
2017-03-23 03:49:47 -0700

    ready for e

--- l2_double_2attn.py
+++ l2_double_2attn.py
@@ -167,15 +167,6 @@ class TFModel(Model):
         tf.summary.scalar(FILE_TBOARD_LOG + 'Loss per Batch', loss)
         return loss
 
-    def train_on_batch(self, sess, merged, questions_batch, passages_batch, start_token_batch, dropout, answers_batch, mask_batch):
-        """Perform one step of gradient descent on the provided batch of data."""
-        feed = self.create_feed_dict(questions_batch, passages_batch, start_token_batch, dropout, mask_batch, answers_batch=answers_batch)
-        #_, loss = sess.run([self.train_op, self.loss], feed_dict=feed)
-        summary, _, loss, self.last_preds = sess.run([merged, self.train_op, self.loss, self.pred], feed_dict=feed)
-        self.train_writer.add_summary(summary, self.step)
-        self.step += 1
-        return loss
-
     def add_training_op(self, loss):        
         optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
         tf.summary.scalar(FILE_TBOARD_LOG + 'LEARNING_RATE', loss)
